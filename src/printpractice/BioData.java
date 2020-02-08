383c1948139da32e2973f30d511f81cff05e80ec
DilipAkbari <akbaridb@gmail.com>
2020-02-08 16:18:11 +0000

    Java Practice Project

---
+++ src/printpractice/BioData.java
@@ -0,0 +1,13 @@
+package printpractice;
+
+public class BioData {
+    public static void main(String[] args){
+
+        System.out.println("Name=Dilip  ");
+        System.out.println("Surmane=Akbari");
+        System.out.println("Address=Harrow");
+
+
+
+    }
+}
